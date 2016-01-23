ff15ed02af634f10b68d8f4846b522bda1fd84bb
Hilmar Lapp <hlapp@drycafe.net>
2016-01-23 13:35:07 -0500

    Adds documentation to the cleanup script

--- cleanup-socket.sh
+++ cleanup-socket.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-
+#
+# Cleans up (removes) the unix socket used by gcp-cups-connector if somehow the program
+# didn't have a chance to do so itself on orderly shutdown, such as after a power outage.
+#
+# Accepts the location of the config file and the full path to the gcp-cups-connector
+# executable as first and second arguments, respectively. Both have defaults, though.
+# The full program path is needed to determine whether the program is running or not.
+#
+# This can be set as the ExecStartPre task in the systemd.service description.
+#
 set -e
 
 socket_key=monitor_socket_filename
